2a0c7657cf22e128c29a6ba250da5e44d89a21ea
taniacristina <roc.ta.cris@gmail.com>
2019-11-26 07:15:18 -0300

    Tentativa de chamar a página 2 através da página 1

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
+import {Page2} from '../page2/page2'
 
 @IonicPage()
 @Component({
@@ -7,9 +8,13 @@ import { IonicPage, NavController } from 'ionic-angular';
   templateUrl: 'page1.html'
 })
 export class Page1 {
-
+  page2 = Page2;
   constructor(public navCtrl: NavController) {
 
+  }
+
+  outraPagina() {
+    // console.log("Indo para outra página")
   }
 
 }
